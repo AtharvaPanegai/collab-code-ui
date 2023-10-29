71b67f7d30c671e36b87fe86952a7c169ba7426a
AtharvaPanegai <panegai.atharv@gmail.com>
2023-10-29 23:32:01 +0530

    adding new edior page and home(join room) page

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import JoinRoomComponent from '../Components/JoinRoomComponent'
+
+const HomePage = () => {
+    return (
+        <div>
+            <JoinRoomComponent />
+        </div>
+    )
+}
+
+export default HomePage
